b85a19c08b5cbfed76956440d8d170c5b214be71
kouoslproje <46405953+kouoslproje@users.noreply.github.com>
2019-01-05 23:02:52 +0300

    Add files via upload

---
+++ controllers/console/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\cysec\controllers\console;
+
+
+/**
+ * Default controller for the `cysec` module
+ */
+class DefaultController extends \kouosl\base\controllers\console\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
---
+++ controllers/frontend/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\cysec\controllers\frontend;
+
+
+/**
+ * Default controller for the `cysec` module
+ */
+class DefaultController extends \kouosl\base\controllers\frontend\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
